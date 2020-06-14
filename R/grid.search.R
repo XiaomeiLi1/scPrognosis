7d47f3c8e06060ed68597be89d67aa13ed4f7f89
XiaomeiLi1 <40169591+XiaomeiLi1@users.noreply.github.com>
2020-06-15 09:03:07 +0930

    Update grid.search.R

--- R/grid.search.R
+++ R/grid.search.R
@@ -4,7 +4,7 @@ grid.search <- function(ranking1 = NULL, ranking2 = NULL, ranking3 = NULL, N = 5
     for(a2 in seq(0,1-a1,0.1)){
       a3 = 1 - (a1+a2)
       ranking = geneRank(ranking1, ranking2, ranking3,a1,a2,a3)
-      temp = benchdb(ranking)
+      temp = benchdb(ranking, N)
       if(is.null(res)) res = temp
       else res = cbind(res, temp)
     }
