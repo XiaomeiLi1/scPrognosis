7ac84ed55d4737957b0d8a4ee31849c4ab2de6f3
XiaomeiLi1 <40169591+XiaomeiLi1@users.noreply.github.com>
2020-06-15 09:04:32 +0930

    Update benchdb.R

--- R/benchdb.R
+++ R/benchdb.R
@@ -1,30 +1,30 @@
-benchdb <- function(ranking = NULL) {
+benchdb <- function(ranking = NULL, N = 50) {
   pd = survival_data[,c(5,7)] # os
-  A = run_topN(mRNA = RNASeq_data, pd = pd, ranking = ranking)
+  A = run_topN(mRNA = RNASeq_data, pd = pd, ranking = ranking,N = N)
   
   pd = survival_data[,c(6,7)] #rf
-  A = rbind(A,run_topN(mRNA = RNASeq_data, pd = pd, ranking = ranking))
+  A = rbind(A,run_topN(mRNA = RNASeq_data, pd = pd, ranking = ranking,N = N))
   
   ###OS METABRIC
   survival_data = pData(METABRIC)
   pd = survival_data[,c(3,4)]
   pd[,2]= as.numeric(pd[,2])
   pd[which(pd[,2] == 2),2] <- 0
-  A = rbind(A,run_topN(mRNA = exprs(METABRIC), pd = pd, ranking = ranking))
+  A = rbind(A,run_topN(mRNA = exprs(METABRIC), pd = pd, ranking = ranking,N = N))
   
   ####RF
   pd = survival_data[,c(5,6)]
-  A = rbind(A,run_topN(mRNA = exprs(METABRIC), pd = pd, ranking = ranking))
+  A = rbind(A,run_topN(mRNA = exprs(METABRIC), pd = pd, ranking = ranking,N = N))
   
   ###GEO RF
   survival_data = pData(GEO)
   pd = survival_data[,c(11,12)]
   pd = apply(pd, 2, as.numeric)
-  A = rbind(A,run_topN(mRNA = exprs(GEO), pd = pd, ranking = ranking))
+  A = rbind(A,run_topN(mRNA = exprs(GEO), pd = pd, ranking = ranking,N = N))
   
   ###UK DFS
   survival_data = pData(UK)
   pd = survival_data[,c(20,19)] #rfs
-  A = rbind(A,run_topN(mRNA = exprs(UK), pd = pd, ranking = ranking))
+  A = rbind(A,run_topN(mRNA = exprs(UK), pd = pd, ranking = ranking,N = N))
   return(A)
 }
